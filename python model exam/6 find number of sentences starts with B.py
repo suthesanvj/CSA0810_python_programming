edcf78c8eaa513cac98d9910742b90961a445a4d
suthesan vj <118433249+suthesanvj@users.noreply.github.com>
2023-10-25 05:19:05 -0400

    Add files via upload

---
+++ python model exam/6 find number of sentences starts with B.py
@@ -0,0 +1,11 @@
+string=input("Enter the Para: ")
+str1=string.split(" ...")
+str1.pop()
+print("Total number of lines:",len(str1))
+count=0
+for i in str1:
+ str2=i.split()
+ for j in str2:
+  if j[0]=="B":
+   count=count+1
+print("Number of Sentences that start with letter B :",count)
